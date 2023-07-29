71044ff39b55d0c976e477524b98f8fbfd1be203
nahid18 <nahidpatwary50@gmail.com>
2023-07-29 21:56:46 +0600

    feat: error handling

--- easylift.R
+++ easylift.R
@@ -7,23 +7,39 @@ easylift <- function(gr, to, chain) {
       "Error: The genome information for the 'gr' object is missing (NA). Please set the genome before using easylift."
     )
   }
-  ch <- rtracklayer::import.chain(chain)
-  seqlevelsStyle(gr) <- "UCSC"
+  # Convert the input GRanges to the "UCSC" seqlevels style if not already
+  if (seqlevelsStyle(gr) != "UCSC") {
+    seqlevelsStyle(gr) <- "UCSC"
+  }
   
-  cur <- unlist(rtracklayer::liftOver(gr, ch))
+  # Load the chain file
+  ch <- import.chain(chain)
+  
+  # Check if the 'to' genome is valid and available
+  to_genome_info <- getChromInfoFromUCSC(to, assembled.molecules.only = TRUE, as.Seqinfo = TRUE)
+  if (is.null(to_genome_info)) {
+    stop(
+      paste("Error: The genome", to, "is not available or recognized.")
+    )
+  }
+  
+  # LiftOver the genomic coordinates
+  cur <- unlist(liftOver(gr, ch))
   genome(cur) <- to
   
-  to_seqinfo <-
-    getChromInfoFromUCSC(to,
-                         assembled.molecules.only = TRUE,
-                         as.Seqinfo = TRUE)
+  # Update the seqinfo with the target genome information
   lifted_seqlevels <- seqlevels(cur)[genome(cur) %in% to]
-  seqinfo(cur) <- update(seqinfo(cur), to_seqinfo[lifted_seqlevels])
+  if (length(lifted_seqlevels) == 0) {
+    stop("Error: LiftOver did not produce any valid results for the target genome.")
+  }
+  to_seqinfo <- to_genome_info[lifted_seqlevels]
+  seqinfo(cur) <- update(seqinfo(cur), to_seqinfo)
+  
   return(cur)
 }
 
 gr <- GRanges(seqname = Rle(paste("chr", 1, sep = "")),
-              ranges = IRanges(start = 266038, end = 16207633))
+              ranges = IRanges(start = 1, end = 200000))
 genome(gr) <- "hg19"
 to <- "hg38"
 chain <- "data/hg19ToHg38.over.chain"
